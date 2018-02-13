df9709ba94545ca191f45bdbdcdeaefb75d17aac
CyborgCats4256 <xxx@yyy.zzz>
2018-02-13 16:48:15 -0600

    Ian, Michael, Matthew, Miles: added space to if statements, added miles' tx2 code, fixed clamp open and close by readding second solenoid, added clamp code robot.java.

--- src/org/usfirst/frc/team4256/robot/R_Clamp.java
+++ src/org/usfirst/frc/team4256/robot/R_Clamp.java
@@ -10,16 +10,18 @@ public class R_Clamp {
 	
 	private VictorSPX intakeLeft;
 	private VictorSPX intakeRight;
-	private DoubleSolenoid clamp;
+	private DoubleSolenoid leftClamp;
+	private DoubleSolenoid rightClamp;
 	private DigitalInput sensor;
 	private boolean isClampClosed = true;//TODO private?
 	private boolean hasCube = true;//TODO private?
 	private double intakeConstant = 0.5;//TODO test
 	
-	public R_Clamp(final int intakeLeftID, final int intakeRightID, final DoubleSolenoid clamp, final int sensorID) {
+	public R_Clamp(final int intakeLeftID, final int intakeRightID, final DoubleSolenoid leftClamp, final DoubleSolenoid rightClamp, final int sensorID) {
 		intakeLeft = new VictorSPX(intakeLeftID);
 		intakeRight = new VictorSPX(intakeRightID);
-		this.clamp = clamp;
+		this.leftClamp = leftClamp;
+		this.rightClamp = rightClamp;
 		sensor = new DigitalInput(sensorID);
 	}
 	
@@ -27,7 +29,7 @@ public class R_Clamp {
 	 * This function "slurps" (intakes) the cube into "clampy"
 	**/
 	public void slurp() {
-		if(!sensor.get()) {
+		if (!sensor.get()) {
 			intakeLeft.set(ControlMode.PercentOutput, -intakeConstant);//TODO negative?
 			intakeRight.set(ControlMode.PercentOutput, -intakeConstant);//TODO negative?
 			hasCube = false;
@@ -50,17 +52,19 @@ public class R_Clamp {
 	/**
 	 * This function closes the clamp 
 	**/
-	public void closeClamp() {
-		clamp.set(DoubleSolenoid.Value.kReverse);//TODO test
-		isClampClosed = false;
+	public void close() {
+		leftClamp.set(DoubleSolenoid.Value.kReverse);//TODO test (if wrong switch to kForward)
+		rightClamp.set(DoubleSolenoid.Value.kReverse);//TODO test (if wrong switch to kForward)
+		isClampClosed = false;//TODO test could be reversed
 	}
 	
 	/**
 	 * This function opens the clamp 
 	**/
-	public void openClamp() {
-		clamp.set(DoubleSolenoid.Value.kForward);//TODO test
-		isClampClosed = true;
+	public void open() {
+		leftClamp.set(DoubleSolenoid.Value.kForward);//TODO test (if wrong switch to kReverse)
+		rightClamp.set(DoubleSolenoid.Value.kForward);//TODO test (if wrong switch to kReverse)
+		isClampClosed = true; //TODO test cold be reversed
 	}
 	
 	/**
