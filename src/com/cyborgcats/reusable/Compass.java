a1918d88ff8b57fda499a2edaabc62820f8957cb
Hayden Shively <haydenshively@gmail.com>
2018-07-06 21:06:45 -0500

    Finished java docs in Compass

--- src/com/cyborgcats/reusable/Compass.java
+++ src/com/cyborgcats/reusable/Compass.java
@@ -1,7 +1,7 @@
 package com.cyborgcats.reusable;//COMPLETE 2016
 
 public class Compass {
-	private double tareAngle = 0;
+	private double tareAngle = 0.0;
 	private final double protectedZoneStart;//Angles increase as the numbers on a clock increase. This value should be the first protected angle encountered by a minute hand which starts at 12:00.
 	private final double protectedZoneSize;//This value should be the number of degrees the minute hand must travel before reaching the end of the protected section.
 	
@@ -12,21 +12,23 @@ public class Compass {
 	
 	
 	/**
-	 * This function tares the compass at the specified angle, relative to the current 0. It accepts both -'s and +'s.
-	**/
+	 * Tares the compass relative to the current 0
+	 * @param tareAngle the new tare angle; can be positive or negative
+	 */
 	public void setTareAngle(final double tareAngle) {this.tareAngle = tareAngle;}
 	
 	
 	/**
-	 * This function returns the current tare angle, relative to the original 0.
-	 * It does not indicate the tare angle relative to a previous tare angle.
-	**/
+	 * @return the current tare angle relative to original 0, not relative to a previous tare
+	 */
 	public double getTareAngle() {return tareAngle;}
 	
 	
 	/**
-	 * This function modifies the input to create a value between 0 and 359.999...
-	**/
+	 * Wraps values around a circle
+	 * @param angle degrees; can be positive, negative, huge, or tiny
+	 * @return the corresponding angle in the range [0, 360)
+	 */
 	public static double validate(final double angle) {
 		final double temp = 360 - (Math.abs(angle)%360);
 		if (angle < 0) return (temp < 360) ? temp : 0;
@@ -35,15 +37,22 @@ public class Compass {
 	
 	
 	/**
-	 * This function finds the shortest path from the start angle to the end angle and returns the size of that path in degrees.
-	 * Positive means clockwise and negative means counter-clockwise.
-	**/
+	 * Finds the measure of the minor arc between two points on a circle
+	 * @param start the first point, designated in degrees
+	 * @param end the second point, designated in degrees
+	 * @return arc measure in degrees (positive if the arc is clockwise of start, negative if it's counterclockwise of start)
+	 */
 	public static double path(final double start, final double end) {return Math.IEEEremainder(end - start, 360.0);}
 	
 	
 	/**
-	 * This function returns a valid and legal version of the input.
-	**/
+	 * Calls <code>validate(angle)</code>. If the result is inside the protected zone,
+	 * this method pushes it out to the nearest zone border.
+	 * @param angle degrees; can be positive, negative, huge, or tiny
+	 * @return the corresponding angle in the range [0, 360) \ (zone start, zone end)<br>
+	 * this denotes angles in [0, 360) but outside (zone start, zone end)
+	 * @see #validate(angle)
+	 */
 	public double legalize(double angle) {
 		if (protectedZoneSize != 0) {
 			final double protectedZoneEnd = protectedZoneStart + protectedZoneSize;
@@ -61,8 +70,11 @@ public class Compass {
 	
 	
 	/**
-	 * This function returns the path to the border that is nearest to the specified angle.
-	**/
+	 * Uses <code>path(start, end)</code> to find the minor arc measure between start and the nearest zone border
+	 * @param start degrees; can be positive, negative, huge, or tiny
+	 * @return arc measure in degrees (positive if the arc is clockwise of start, negative if it's counterclockwise of start)
+	 * @see #path(start, end)
+	 */
 	private double borderPath(final double start) {
 		final double toStartingEdge = path(start, protectedZoneStart),
 					 toEndingEdge = path(start, protectedZoneStart + protectedZoneSize);
@@ -74,6 +86,16 @@ public class Compass {
 	 * This function finds the shortest legal path from the start angle to the end angle and returns the size of that path in degrees.
 	 * Positive means clockwise and negative means counter-clockwise.
 	**/
+	/**
+	 * Uses <code>path(start, end)</code> to find the smallest arc between start and <code>legalize(end)</code> that<br>
+	 * doesn't intersect the protected zone. If start was in that zone, <code>borderPath(start)</code> is added to the result
+	 * @param start the first point, designated in degrees
+	 * @param end the second point, designated in degrees
+	 * @return arc measure in degrees (positive if the arc is clockwise of start, negative if it's counterclockwise of start)
+	 * @see #path(angle1, angle2)
+	 * @see #legalize(angle)
+	 * @see #borderPath(angle)
+	 */
 	public double legalPath(final double start, final double end) {
 		final double start_legal = legalize(start);
 		final double path_escape = validate(start) == start_legal ? 0.0 : borderPath(start);//0 if start was already legal, otherwise borderPath()
@@ -101,9 +123,10 @@ public class Compass {
 	
 	
 	/**
-	 * This function returns the standard deviation of an array of angles in degrees.
-	 * It's smart enough to handle 360-0 boundary condition.
-	**/
+	 * Calculates the standard deviation of an array of angles (can handle 360->0 boundary condition)
+	 * @param angles an array of angles in degrees
+	 * @return the standard deviation in degrees
+	 */
 	public static double stdd(final double[] angles) {
 		double sin = 0.0,	cos = 0.0;
 		for (double angle : angles) {sin += Math.sin(Math.toRadians(angle));cos += Math.cos(Math.toRadians(angle));}
@@ -117,7 +140,9 @@ public class Compass {
 	
 	
 	/**
-	 * This function finds the angle between the Y axis and any Cartesian coordinate.
-	**/
+	 * @param x x coordinate
+	 * @param y y coordinate
+	 * @return the angle between the Y axis and (x, y) measured in degrees
+	 */
 	public static double convertToAngle(final double x, final double y) {return Math.toDegrees(Math.atan2(x, -y));}
 }
